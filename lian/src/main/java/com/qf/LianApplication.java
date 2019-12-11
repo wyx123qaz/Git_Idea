f5bb73d479535a8d48e6f6c82121b8f6a15c1e86
武永鑫 <3119064605@qq.com>
2019-12-11 11:56:19 +0800

    提交A

---
+++ lian/src/main/java/com/qf/LianApplication.java
@@ -0,0 +1,13 @@
+package com.qf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LianApplication.class, args);
+    }
+
+}
